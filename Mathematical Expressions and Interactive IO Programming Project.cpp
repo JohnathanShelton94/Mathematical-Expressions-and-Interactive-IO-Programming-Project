1639c0af74dd04937c72d0a156f34c02e2b1e593
Johnathan Shelton <js24634@email.vccs.edu>
2024-09-18 18:42:27 -0400

    Add project files.

---
+++ Mathematical Expressions and Interactive IO Programming Project.cpp
@@ -0,0 +1,9 @@
+// Mathematical Expressions and Interactive IO Programming Project.cpp : This file contains the Mathematical Expressions and Interactive IO Programming Projects
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Mathematical Expressions and Interactive IO Programming Project master branch\n";
+}
